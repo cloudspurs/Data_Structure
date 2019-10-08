6adc2503b04ade9816cd4aab53422fb46c5657ff
mengqinggang <mqg@email.com>
2019-10-08 19:32:21 +0800

    03

--- chapt03/calc/easy/CALCU.C
+++ chapt03/calc/easy/CALCU.C
@@ -7,83 +7,74 @@
 
 void main()
 {
- float f;
- char str[80]={0};
- int key,len,i;
- char keystr[]="1234567890.^()+-*/=\x08";
+    float f;
+    char str[80] = { 0 };
+    int key, len, i;
+    char keystr[] = "1234567890.^()+-*/=\x08";
 
- _setcursortype(_NOCURSOR);
- clrscr();
- gotoxy(1,2);
- printf("ÚÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ¿\n");
- printf("³                         ³\n");
- printf("ÃÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ´\n");
- printf("³  1   2   3     +   -    ³\n");
- printf("³                         ³\n");
- printf("³  4   5   6     *   /    ³\n");
- printf("³                         ³\n");
- printf("³  7   8   9     =   \x1b    ³\n");
- printf("³                         ³\n");
- printf("³  0   .         ^   End  ³\n");
- printf("ÀÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÙ\n");
+    _setcursortype(_NOCURSOR);
+    clrscr();
+    gotoxy(1, 2);
+    printf("ÚÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ?n");
+    printf("?                        ?n");
+    printf("ÃÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ?n");
+    printf("? 1   2   3     +   -    ?n");
+    printf("?                        ?n");
+    printf("? 4   5   6     *   /    ?n");
+    printf("?                        ?n");
+    printf("? 7   8   9     =   \x1b    ?n");
+    printf("?                        ?n");
+    printf("? 0   .         ^   End  ?n");
+    printf("ÀÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄÄ?n");
 
-  while(1)
-  {
-   if(kbhit())
-   {
-	key=bioskey(0);
-	if(key==END)break;
-	if(strlen(str)==0)
-	{
-	gotoxy(2,3);
-	printf("                         ");
-	  }
-	key&=0x00ff;
-	len=strlen(keystr);
-	for(i=0;i<len;i++)
-	  if(key==keystr[i])break;
-	if(i==len)
-	 {/*illegal input*/
-	  printf("\07");
-	  continue;
-	   }
-	if(key!='=')
-	{
-	 len=strlen(str);
-	 if(key==8)
-	  {
-	   if(len>0)
-	   {
-	   gotoxy(2+len-1,3);
-	   printf(" ");
-	   str[len-1]=0;
-		  }
-		}
-	 else
-	 {
-	  str[len]=key;
-	  str[len+1]=0;
-	  }
-	 gotoxy(2,3);
-	 printf("%s",str);
-	 }
-	else
-	{//begin calculate the value of the expression
-	 if(str[0]!=0)
-	 {
-		len=strlen(str);
-		str[len]='=';
-		str[len+1]=0;
-		if(Evaluate(str,&f))
-		{
-		 gotoxy(2,3);
-		 printf("%s%.2f",str,f);
-		   }
-		}
-	 str[0]=0;
-	  }
-	} //if kbhit
-   }//while
- _setcursortype(_NORMALCURSOR);
- gotoxy(1,13);
- }
+    while (1) {
+        if (kbhit()) {
+            key = bioskey(0);
+            if (key == END)
+                break;
+            if (strlen(str) == 0) {
+                gotoxy(2, 3);
+                printf("                         ");
+            }
+            key &= 0x00ff;
+            len = strlen(keystr);
+            for (i = 0; i < len; i++)
+                if (key == keystr[i])
+                    break;
+            if (i == len) { /*illegal input*/
+                printf("\07");
+                continue;
+            }
+            if (key != '=') {
+                len = strlen(str);
+                if (key == 8) {	// backspace
+                    if (len > 0) {
+                        gotoxy(2 + len - 1, 3);
+                        printf(" ");
+                        str[len - 1] = 0;
+                    }
+                }
+                else {
+                    str[len] = key;
+                    str[len + 1] = 0;
+                }
+                gotoxy(2, 3);
+                printf("%s", str);
+            }
+            else { //begin calculate the value of the expression
+                if (str[0] != 0) {
+                    len = strlen(str);
+                    str[len] = '=';
+                    str[len + 1] = 0;
+                    if (Evaluate(str, &f)) {
+                        gotoxy(2, 3);
+                        printf("%s%.2f", str, f);
+                    }
+                }
+                str[0] = 0;
+            }
+        } //if kbhit
+    } //while
+    _setcursortype(_NORMALCURSOR);
+    gotoxy(1, 13);
+}
--- chapt03/calc/easy/EVAL.C
+++ chapt03/calc/easy/EVAL.C
@@ -3,149 +3,156 @@
 #include <math.h>
 #include "sstack.h"
 
+WORD GetWord(char* str, int* k)
+{
+    int i = *k, j;
+    WORD w;
+    char tmpstr[40];
+    float f;
 
-WORD GetWord(char *str,int *k)
-{int i=*k,j;
- WORD w;
- char tmpstr[40];
- float f;
-
- w.type=ERROR;
- if(str[i]==0)return w;
- else if(str[i]=='.'||(str[i]>='0'&&str[i]<='9'))
- {//get the operand
-  j=0;
-  while(str[i]=='.'||(str[i]>='0'&&str[i]<='9'))
-   tmpstr[j++]=str[i++];
-  tmpstr[j]=0;
-  *k=i;
-  sscanf(tmpstr,"%f",&f);
-  w.type=OPERAND;
-  w.word.X=f;
-  }
- else if(str[i]=='+'||str[i]=='-'||str[i]=='*'||str[i]=='/'||str[i]=='^'||str[i]=='('||str[i]==')'||str[i]=='=')
- {//get the operator
-  w.type=OPERATOR;
-  w.word.op=str[i];
-  *k=i+1;
-  }
- return w;
- }
+    w.type = ERROR;
+    if (str[i] == 0)
+        return w;
+    else if (str[i] == '.' || (str[i] >= '0' && str[i] <= '9')) { //get the operand
+        j = 0;
+        while (str[i] == '.' || (str[i] >= '0' && str[i] <= '9'))
+            tmpstr[j++] = str[i++];
+        tmpstr[j] = 0;
+        *k = i;
+        sscanf(tmpstr, "%f", &f);
+        w.type = OPERAND;
+        w.word.X = f;
+    }
+    else if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/' || str[i] == '^' || str[i] == '(' || str[i] == ')' || str[i] == '=') { //get the operator
+        w.type = OPERATOR;
+        w.word.op = str[i];
+        *k = i + 1;
+    }
+    return w;
+}
 int InPri(char op)
 {
- char operators[7]="(+-*/^";
- int  pri[6]={0,2,2,4,4,5};
- int i=0;
- while(operators[i]!=0)
-   if(operators[i]==op)
-	   return pri[i];
-   else i++;
- return -1;
- }
+    char operators[7] = "(+-*/^";
+    int pri[6] = { 0, 2, 2, 4, 4, 5 };
+    int i = 0;
+    while (operators[i] != 0)
+        if (operators[i] == op)
+            return pri[i];
+        else
+            i++;
+    return -1;
+}
 int OutPri(char op)
 {
- char operators[7]="(+-*/^";
- int  pri[6]={7,1,1,3,3,6};
- int i=0;
- while(operators[i]!=0)
-   if(operators[i]==op)
-	   return pri[i];
-   else i++;
- return -1;
- }
-float comput(float x,char op,float y)
+    char operators[7] = "(+-*/^";
+    int pri[6] = { 7, 1, 1, 3, 3, 6 };
+    int i = 0;
+    while (operators[i] != 0)
+        if (operators[i] == op)
+            return pri[i];
+        else
+            i++;
+    return -1;
+}
+float comput(float x, char op, float y)
 {
- switch(op)
- {
-  case '+':return x+y;
-  case '-':return x-y;
-  case '*':return x*y;
-  case '/':return x/y;
-  case '^':return pow(x,y);
-  }
- }
-int Evaluate(char *exp, float *result)
+    switch (op) {
+    case '+':
+        return x + y;
+    case '-':
+        return x - y;
+    case '*':
+        return x * y;
+    case '/':
+        return x / y;
+    case '^':
+        return pow(x, y);
+    }
+}
+int Evaluate(char* exp, float* result)
 {
-   int k=0;
-   WORD w,W1,W2,W3;
-   SQSTACK s1,s2;
-   *result=0;
-   InitSqstack(&s1,50);
-   InitSqstack(&s2,50);
-   w.word.op='(';
-   Push(&s2,w);
-   while(1){
-      w=GetWord(exp,&k);
-      if(w.type==ERROR)  return 0;
-      else if(w.type==OPERAND)  Push(&s1,w);
-      else if(w.type==OPERATOR)
-	 switch(w.word.op){
-	    case ')':
-	       while(s2.elem[s2.top].word.op!='('){
-		  if(!Pop(&s1,&W2)||!Pop(&s1,&W1)||!Pop(&s2,&W3)){
-		     DestroySqstack(&s1);
-		     DestroySqstack(&s2);
-		     return 0;
-		  }
-		  if(!(W2.word.X==0&&W3.word.op=='/')){
-		     W1.word.X=comput(W1.word.X,W3.word.op,W2.word.X);
-		     Push(&s1,W1);
-		  }
-		  else{
-		     DestroySqstack(&s1);
-		     DestroySqstack(&s2);
-		     return 0;
-		  }
-	       }
-	       Pop(&s2,&W3);
-	       break;
-	    case'=':
-	       while(s2.elem[s2.top].word.op!='('){
-		  if(!Pop(&s1,&W2)||!Pop(&s1,&W1)||!Pop(&s2,&W3)){
-		     DestroySqstack(&s1);
-		     DestroySqstack(&s2);
-		     return 0;
-		  }
-		  if(!(W2.word.X==0&&W3.word.op=='/')){
-		     W1.word.X=comput(W1.word.X,W3.word.op,W2.word.X);
-		     Push(&s1,W1);
-		  }
-		  else{
-		     DestroySqstack(&s1);
-		     DestroySqstack(&s2);
-		     return 0;
-		  }
-	       }
-		*result=s1.elem[s1.top].word.X;
-		if(s1.top!=0){
-		   DestroySqstack(&s1);
-		   DestroySqstack(&s2);
-		   return 0;
-	    }
-		else{
-		   DestroySqstack(&s1);
-		   DestroySqstack(&s2);
-		   return 1;
-	    }
-	    default:
-	       while(InPri(s2.elem[s2.top].word.op)>OutPri(w.word.op)){
-		  if(!Pop(&s1,&W2)||!Pop(&s1,&W1)||!Pop(&s2,&W3)){
-		     DestroySqstack(&s1);
-		     DestroySqstack(&s2);
-		     return 0;
-		  }
-		  if(!(W2.word.X==0&&W3.word.op=='/')){
-		     W1.word.X=comput(W1.word.X,W3.word.op,W2.word.X);
-		     Push(&s1,W1);
-		  }
-		  else{
-		     DestroySqstack(&s1);
-		     DestroySqstack(&s2);
-		     return 0;
-		  }
-	       }
-	       Push(&s2,w);
-	       break;
-	 }
-   }
- }
+    int k = 0;
+    WORD w, W1, W2, W3;
+    SQSTACK s1, s2;
+    *result = 0;
+    InitSqstack(&s1, 50);
+    InitSqstack(&s2, 50);
+    w.word.op = '(';
+    Push(&s2, w);
+    while (1) {
+        w = GetWord(exp, &k);
+        if (w.type == ERROR)
+            return 0;
+        else if (w.type == OPERAND)
+            Push(&s1, w);
+        else if (w.type == OPERATOR)
+            switch (w.word.op) {
+            case ')':
+                while (s2.elem[s2.top].word.op != '(') {
+                    if (!Pop(&s1, &W2) || !Pop(&s1, &W1) || !Pop(&s2, &W3)) {
+                        DestroySqstack(&s1);
+                        DestroySqstack(&s2);
+                        return 0;
+                    }
+                    if (!(W2.word.X == 0 && W3.word.op == '/')) {
+                        W1.word.X = comput(W1.word.X, W3.word.op, W2.word.X);
+                        Push(&s1, W1);
+                    }
+                    else {
+                        DestroySqstack(&s1);
+                        DestroySqstack(&s2);
+                        return 0;
+                    }
+                }
+                Pop(&s2, &W3);
+                break;
+            case '=':
+                while (s2.elem[s2.top].word.op != '(') {
+                    if (!Pop(&s1, &W2) || !Pop(&s1, &W1) || !Pop(&s2, &W3)) {
+                        DestroySqstack(&s1);
+                        DestroySqstack(&s2);
+                        return 0;
+                    }
+                    if (!(W2.word.X == 0 && W3.word.op == '/')) {
+                        W1.word.X = comput(W1.word.X, W3.word.op, W2.word.X);
+                        Push(&s1, W1);
+                    }
+                    else {
+                        DestroySqstack(&s1);
+                        DestroySqstack(&s2);
+                        return 0;
+                    }
+                }
+                *result = s1.elem[s1.top].word.X;
+                if (s1.top != 0) {
+                    DestroySqstack(&s1);
+                    DestroySqstack(&s2);
+                    return 0;
+                }
+                else {
+                    DestroySqstack(&s1);
+                    DestroySqstack(&s2);
+                    return 1;
+                }
+            default:
+                while (InPri(s2.elem[s2.top].word.op) > OutPri(w.word.op)) {
+                    if (!Pop(&s1, &W2) || !Pop(&s1, &W1) || !Pop(&s2, &W3)) {
+                        DestroySqstack(&s1);
+                        DestroySqstack(&s2);
+                        return 0;
+                    }
+                    if (!(W2.word.X == 0 && W3.word.op == '/')) {
+                        W1.word.X = comput(W1.word.X, W3.word.op, W2.word.X);
+                        Push(&s1, W1);
+                    }
+                    else {
+                        DestroySqstack(&s1);
+                        DestroySqstack(&s2);
+                        return 0;
+                    }
+                }
+                Push(&s2, w);
+                break;
+            }
+    }
+}
